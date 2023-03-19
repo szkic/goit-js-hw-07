89d74d3fd4c28edcd4f89ac57b710bc4503615f8
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-03-19 16:11:21 +0100

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const addImages = galleryItems.map((image) => {
+  const createEl = document.createElement("li");
+  const createLink = document.createElement("a");
+  const createImg = document.createElement("img");
+
+  createEl.classList.add("gallery__item");
+  createEl.append(createLink);
+
+  createLink.classList.add("gallery__link");
+  createLink.setAttribute("href", image.original);
+  createLink.append(createImg);
+
+  createLink.addEventListener("click", (event) => event.preventDefault());
+
+  createImg.classList.add("gallery__image");
+  createImg.setAttribute("src", image.preview);
+  createImg.setAttribute("alt", image.description);
+
+  galleryEl.append(createEl);
+
+  console.log(galleryEl);
+});
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: (e) => e.firstChild,
+  captionPosition: "outside",
+  captionsData: "alt",
+  captionDelay: 250,
+  heightRatio: 0.85,
+});
